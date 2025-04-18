51180d6a46f345a3ccf253258b4b8a49f1c3570f
Fernando <manuelhola9@gmail.com>
2025-04-18 12:40:01 -0700

    Mejorar la estructura y funcionalidad de la barra lateral
    Se ha añadido un título dinámico en `_Layout.cshtml`.
    La barra lateral ha sido reestructurada con un nuevo diseño que incluye un botón para mostrar/ocultar, un menú de navegación y un área de búsqueda.
    
    Se han implementado nuevos estilos CSS para animaciones y transiciones en `site.css`, incluyendo las animaciones `swing` y `sonar`.
    
    Además, se ha actualizado `site.js` para manejar la interacción de la barra lateral con jQuery, eliminando el código anterior relacionado con el botón de hamburguesa.

--- Bilioteca/wwwroot/js/site.js
+++ Bilioteca/wwwroot/js/site.js
@@ -1,5 +1,31 @@
-﻿const hamBurger = document.querySelector(".toggle-btn");
+﻿jQuery(function ($) {
 
-hamBurger.addEventListener("click", function () {
-    document.querySelector("#sidebar").classList.toggle("expand");
+    $(".sidebar-dropdown > a").click(function () {
+        $(".sidebar-submenu").slideUp(200);
+        if (
+            $(this)
+                .parent()
+                .hasClass("active")
+        ) {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this)
+                .parent()
+                .removeClass("active");
+        } else {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this)
+                .next(".sidebar-submenu")
+                .slideDown(200);
+            $(this)
+                .parent()
+                .addClass("active");
+        }
+    });
+
+    $("#close-sidebar").click(function () {
+        $(".page-wrapper").removeClass("toggled");
+    });
+    $("#show-sidebar").click(function () {
+        $(".page-wrapper").addClass("toggled");
+    });
 });
