fe8553b50122753e1527bf5433295ee98e58c9e6
Fernando <manuelhola9@gmail.com>
2025-04-19 11:13:44 -0700

    Añadir catálogo de libros con estilos y funcionalidad
    Se ha implementado un nuevo contenedor en `Index.cshtml` que muestra un catálogo de libros con columnas para título, autor, género, año, disponibilidad y acciones. Se han agregado filas de ejemplo y botones para ver, editar y eliminar libros.
    
    Se han añadido estilos en `catalog.css` para mejorar la presentación de la tabla y el cuerpo de la página, así como un efecto de resaltado al pasar el ratón sobre las filas.
    
    Además, se ha implementado un script en `catalog.js` que inicializa la tabla de libros utilizando DataTables con soporte para el idioma español.

---
+++ Bilioteca/wwwroot/modules/Catalog/js/catalog.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('#catalogoLibros').DataTable({
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.5/i18n/es-ES.json'
+        }
+    });
+});
